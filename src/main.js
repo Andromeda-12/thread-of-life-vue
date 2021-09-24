d30971a6b207039f79749d785110cf126f375e5a
Andrey Froshgaizer <andrei.froschgaizer@yandex.ru>
2021-09-24 10:39:07 +0700

    убрал ненужное

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './assets/scss/base.scss'
 import vuetify from './plugins/vuetify'
+import './assets/scss/base.scss'
 
 Vue.config.productionTip = false
 
